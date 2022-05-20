721db02a0cf5af88124c870b5d4b29b84791a7fb
Per Jansson <per.jansson@reaktor.com>
2022-05-20 10:01:38 +0300

    Remove unused code

--- api/stats.ts
+++
@@ -1,6 +0,0 @@
-import { StatsResponse } from '../types'
-
-export const statsFetcher = async (): Promise<StatsResponse> => {
-  const statsResponse = await fetch(`api/stats`)
-  return (await statsResponse.json()) as StatsResponse
-}
